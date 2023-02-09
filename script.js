189b792cc136d9a1921b465cd55eedee6ba93767
Айрат Халиуллин <clovn@MacBook-Air-Ajrat.local>
2023-02-09 14:18:28 +0300

    update choices

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
-let image = document.getElementsByClassName('input-image');
-image[0].addEventListener("change", function () {
-        
-});
+function change_input(file){
+    if(file){
+        let input_buttons = document.getElementById("buttons")
+        console.log('chfakdka')
+        let input_file = document.createElement('input')
+        input_file.setAttribute('type', 'file')
+        input_file.setAttribute('onchange', 'change_input(this.value)')
+        input_file.setAttribute('accept', 'image/png, image/jpeg, image/jpg')
+        input_file.setAttribute('class', 'file-choice')
+        input_buttons.append(input_file)
+    } else {
+        let input_buttons = document.getElementById('buttons')
+        input_buttons.removeChild(input_buttons.lastChild)
+    }
+}
