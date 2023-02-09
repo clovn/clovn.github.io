ab5339487da7696173a74f51cc526a0a9c25f389
Айрат Халиуллин <clovn@MacBook-Air-Ajrat.local>
2023-02-09 15:26:16 +0300

    actions

--- script.js
+++ script.js
@@ -13,3 +13,13 @@ function change_input(file){
         input_buttons.removeChild(input_buttons.lastChild)
     }
 }
+
+let tg = window.Telegram.WebApp; //получаем объект webapp телеграма 
+
+tg.expand(); //расширяем на все окно  
+
+tg.MainButton.text = "Changed Text"; //изменяем текст кнопки 
+tg.MainButton.setText("Changed Text1"); //изменяем текст кнопки иначе
+tg.MainButton.textColor = "#F55353"; //изменяем цвет текста кнопки
+tg.MainButton.color = "#143F6B"; //изменяем цвет бэкграунда кнопки
+tg.MainButton.setParams({"color": "#143F6B"}); //так изменяются все параметры 
